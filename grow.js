e916e9cead13a1a20b72d42ac14e4aae1f7ba8e5
Chris Thomas <webmaster102@gmail.com>
2015-11-07 23:07:49 -0500

    added add'l section

--- grow.js
+++ grow.js
@@ -3,6 +3,7 @@ function show_div(div_id) {
   document.querySelector('.about').style.display = 'none';
   document.querySelector('.rules').style.display = 'none';
   document.querySelector('.register').style.display = 'none';
+  document.querySelector('.donate').style.display = 'none';
   document.querySelector('.contact').style.display = 'none';
   document.querySelector(div_id).style.display = 'block';
 }
